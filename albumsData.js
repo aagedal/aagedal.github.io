6d1dbbc37988462f90be98467032476649844207
Truls Aagedal <truls.aagedal@icloud.com>
2024-06-23 22:35:12 +0200

    Added a lot of new pictures. Some cleanup of albums and categories.

--- albumsData.js
+++ albumsData.js
@@ -3,7 +3,7 @@ const albumsData = {
     Crime: {
       description:
         "Some photojournalistic work from crime scenes. Gain Map HDR images.",
-      images: ["TRA04291.jpg", "TRA08965.jpg"],
+      images: ["TRA04291.jpg", "TRA08965.jpg", "TRA08492.jpg", "TRA08992.jpg"],
     },
     Politics: {
       description: "People talking a lot.",
@@ -41,8 +41,10 @@ const albumsData = {
     Street: {
       description: "Some pictures from the street of Oslo.",
       images: [
+        "TRA9435.jpg",
         "TRA08676.jpg",
         "TRA0229.jpg",
+        "TRA07033.jpg",
         "TRA0957.jpg",
         "TRA0962.jpg",
         "TRA0977.jpg",
@@ -68,6 +70,10 @@ const albumsData = {
         "TRA03937.jpg",
         "TRA03141.jpg",
         "TRA01088.jpg",
+        "TRA0363.jpg",
+        "TRA02076.jpg",
+        "TRA04086.jpg",
+        "TRA06243.jpg",
       ],
     },
     Parks: {
@@ -79,6 +85,8 @@ const albumsData = {
         "TRA01343.jpg",
         "TRA1975.jpg",
         "TRA1981.jpg",
+        "TRA06838.jpg",
+        "TRA06867.jpg",
       ],
     },
     Pride: {
@@ -92,6 +100,8 @@ const albumsData = {
         "TRA9677.jpg",
         "TRA9879.jpg",
         "TRA9781.jpg",
+        "TRA8142.jpg",
+        "TRA9112.jpg",
       ],
     },
     Other: {
@@ -109,31 +119,109 @@ const albumsData = {
         "TAG02362.jpg",
         "TAG02919.jpg",
         "TAG02996.jpg",
+        "TAG02276.jpg",
+      ],
+    },
+    England: {
+      description: "Some pictures from a vacation to England.",
+      images: ["MG_0855.jpg", "MG_0786.jpg", "MG_0596.jpg", "MG_0857.jpg"],
+    },
+    Italy: {
+      description: "Some pictures from vacation in Italy.",
+      images: [
+        "DSC7267.jpg",
+        "DSC7000.jpg",
+        "DSC7246.jpg",
+        "DSC7142.jpg",
+        "DSC7051.jpg",
+        "DSC7174.jpg",
+        "DSC7151.jpg",
+        "DSC7256.jpg",
+        "DSC7229.jpg",
+        "DSC7144.jpg",
+        "DSC7104.jpg",
+        "DSC7012.jpg",
+      ],
+    },
+    Molde: {
+      description: "Some pictures from Molde, Norway.",
+      images: [
+        "DSC6523.jpg",
+        "DSC6378.jpg",
+        "TRA07500.jpg",
+        "DSC6389.jpg",
+        "TRA04991.jpg",
+        "TRA09865.jpg",
+      ],
+    },
+    Hvaler: {
+      description: "Some pictures from Hvaler, Norway.",
+      images: [
+        "TRA05504.jpg",
+        "TRA05445.jpg",
+        "TRA05517.jpg",
+        "TRA05542.jpg",
+        "TRA08207.jpg",
+      ],
+    },
+    "Planes and Transport": {
+      description:
+        "How did I get where I was going. By taking a lot of pictures to distract me from my fear of flying.",
+      images: [
+        "TRA04946.jpg",
+        "DSC6989.jpg",
+        "TRA07299.jpg",
+        "DSC7310.jpg",
+        "TRA00176.jpg",
+        "TRA09843.jpg",
+        "TRA09852.jpg",
+        "TRA07306.jpg",
+        "TRA05387.jpg",
+        "TRA00089.jpg",
+        "TRA07644.jpg",
+        "TRA00200.jpg",
+        "TRA1088.jpg",
+        "TRA8730.jpg",
+        "TRA00099.jpg",
+        "TRA09810.jpg",
+        "TRA08948.jpg",
+        "TRA08971.jpg",
+        "TRA07647.jpg",
+        "TRA00213.jpg",
+        "TRA00209.jpg",
+        "TRA00208.jpg",
+        "TRA00196.jpg",
+        "TRA9396.jpg",
       ],
     },
   },
   category4: {
-    Spain: {
-      description: "First album of category 4.",
-      images: ["TRA02080.jpeg", "TRA00129.jpeg", "TRA02576-2.jpeg"],
+    Food: {
+      description: "Things I've made or eaten. Probably both.",
+      images: ["TRA00131.jpg", "TRA00075.jpg", "TRA07988.jpg"],
     },
-    Italy: {
-      description: "First album of category 4.",
-      images: ["TRA02080.jpeg", "TRA00129.jpeg", "TRA02576-2.jpeg"],
-    },
-    Molde: {
-      description: "First album of category 4.",
-      images: ["TRA03559.jpg"],
-    },
-  },
-  category5: {
-    Test: {
-      description: "First album of category 4.",
-      images: ["TRA02080.jpeg", "TRA00129.jpeg", "TRA02576-2.jpeg"],
+    "Animals and Insects": {
+      description: "Living non-human (conscious?) creatures.",
+      images: [
+        "TRA02266.jpg",
+        "DSC6788.jpg",
+        "TRA00517.jpg",
+        "DSC7185.jpg",
+        "TRA1977.jpg",
+        "TRA00004.jpg",
+        "DSC00587.jpg",
+        "TRA00481.jpg",
+        "TRA09903.jpg",
+      ],
     },
     "Body Art": {
-      description: "Bodyart (possibly nsfw).",
-      images: [""],
+      description:
+        "Body and soul, in artistic form. May be somewhat NSFW, but probably not.",
+      images: ["TRA9677.jpg"],
+    },
+    Other: {
+      description: "Asorted stuff. WIP. Testing. Not sure where to put these.",
+      images: ["TRA0357.jpg", "TRA2069.jpg"],
     },
   },
 };
